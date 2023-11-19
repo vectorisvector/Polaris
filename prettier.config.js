0bfb68a921b53d55d35dc27d5ea51168ba91c2ed
Vector <vector9527@proton.me>
2023-11-20 02:25:27 +0800

    feat: script

---
+++ prettier.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  plugins: ["prettier-plugin-tailwindcss"],
+  trailingComma: "all",
+  singleAttributePerLine: true,
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,198 @@
-import Image from 'next/image'
+"use client";
+
+import classNames from "classnames";
+import { useCallback, useState } from "react";
+import {
+  Hex,
+  PrivateKeyAccount,
+  createWalletClient,
+  http,
+  isAddress,
+  stringToHex,
+} from "viem";
+import { privateKeyToAccount } from "viem/accounts";
+import { avalanche } from "viem/chains";
+
+const defaultAvalancheRpc =
+  "https://avalanche-mainnet.infura.io/v3/89a0656361da45419291de4bce4ccc62";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+  const [accounts, setAccounts] = useState<PrivateKeyAccount[]>([]);
+  const [toAddress, setToAddress] = useState<Hex>();
+  const [inscription, setInscription] = useState<string>("");
+  const [logs, setLogs] = useState<string[]>([]);
+  const [running, setRunning] = useState<boolean>(false);
+  const [timer, setTimer] = useState<NodeJS.Timeout>();
+  const [rpc, setRpc] = useState<string>();
+  const [intervalTime, setIntervalTime] = useState<number>(1000);
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+  const handleLog = (log: string, state: string = "success") => {
+    return `${new Date().toLocaleString()} ${
+      state === "success" ? "✅" : state === "error" ? "❌" : ""
+    } => ${log}`;
+  };
+
+  const handleAddress = (address: Hex) => {
+    const prefix = address.slice(0, 6);
+    const suffix = address.slice(-4);
+    return `${prefix}...${suffix}`;
+  };
+
+  const run = useCallback(() => {
+    if (accounts.length === 0) {
+      setLogs((logs) => [...logs, handleLog("没有私钥", "error")]);
+      setRunning(false);
+      return;
+    }
+
+    if (!toAddress) {
+      setLogs((logs) => [...logs, handleLog("没有地址", "error")]);
+      setRunning(false);
+      return;
+    }
+
+    if (!inscription) {
+      setLogs((logs) => [...logs, handleLog("没有铭文", "error")]);
+      setRunning(false);
+      return;
+    }
+
+    const client = createWalletClient({
+      chain: avalanche,
+      transport: http(rpc || defaultAvalancheRpc),
+    });
+
+    const timer = setInterval(async () => {
+      for (const account of accounts) {
+        try {
+          const hash = await client.sendTransaction({
+            account,
+            to: toAddress,
+            value: 0n,
+            data: stringToHex(inscription),
+          });
+          setLogs((logs) => [
+            ...logs,
+            handleLog(`${handleAddress(account.address)} ${hash}`, "success"),
+          ]);
+        } catch (error) {
+          setLogs((logs) => [
+            ...logs,
+            handleLog(`${handleAddress(account.address)} error`, "error"),
+          ]);
+        }
+      }
+    }, intervalTime);
+    setTimer(timer);
+  }, [accounts, inscription, intervalTime, rpc, toAddress]);
+
+  return (
+    <main className=" flex flex-col items-center gap-5">
+      <h1 className=" mt-5 text-5xl">Inscription</h1>
+
+      <div className=" mt-3 flex flex-col gap-2">
+        <span>私钥（每行一个）:</span>
+        <textarea
+          className=" h-[100px] w-[800px] rounded-lg border p-2"
+          placeholder="私钥"
+          disabled={running}
+          onChange={(e) => {
+            const text = e.target.value;
+            const lines = text.split("\n");
+            const accounts = lines.map((line) => {
+              const key = "0x" + line.trim();
+              if (/^0x[a-fA-F0-9]{64}$/.test(key)) {
+                return privateKeyToAccount(key as Hex);
+              }
+            });
+            setAccounts(accounts.filter((x) => x) as PrivateKeyAccount[]);
+          }}
         />
       </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      <div className=" mt-3 flex flex-col gap-2">
+        <span>转给谁的地址:</span>
+        <input
+          className=" h-10 w-[800px] rounded-lg border px-2"
+          placeholder="地址"
+          disabled={running}
+          onChange={(e) => {
+            const text = e.target.value;
+            isAddress(text) && setToAddress(text);
+          }}
+        />
+      </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+      <div className=" mt-3 flex flex-col gap-2">
+        <span>自己的 rpc（可选，最好用自己的）:</span>
+        <input
+          className=" h-10 w-[800px] rounded-lg border px-2"
+          placeholder="rpc"
+          disabled={running}
+          onChange={(e) => {
+            const text = e.target.value;
+            setRpc(text);
+          }}
+        />
+      </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+      <div className=" mt-3 flex flex-col gap-2">
+        <span>要打的铭文:</span>
+        <textarea
+          className=" h-[100px] w-[800px] rounded-lg border p-2"
+          placeholder="铭文"
+          disabled={running}
+          onChange={(e) => {
+            const text = e.target.value;
+            setInscription(text);
+          }}
+        />
+      </div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className=" mt-3 flex items-center justify-center gap-5">
+        <button
+          className={classNames(
+            " h-10 w-[200px] rounded-full text-white transition-all hover:opacity-80",
+            running ? " bg-red-600" : " bg-green-600",
+          )}
+          onClick={() => {
+            if (!running) {
+              setRunning(true);
+              run();
+            } else {
+              setRunning(false);
+              timer && clearInterval(timer);
+            }
+          }}
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          {running ? "运行中" : "运行"}
+        </button>
+
+        <input
+          className=" h-10 w-[400px] rounded-lg border px-2"
+          placeholder="间隔时间（默认 1000ms）"
+          type="number"
+          disabled={running}
+          onChange={(e) => {
+            const text = e.target.value;
+            setIntervalTime(Number(text));
+          }}
+        />
+      </div>
+
+      <div className=" mt-5 flex w-[1000px] flex-col gap-2">
+        <span>日志:</span>
+        <div className=" mt-3 flex h-[600px] flex-col gap-1 overflow-auto rounded-lg bg-gray-100 px-4 py-2">
+          {logs.map((log, index) => (
+            <div
+              key={log + index}
+              className=" flex h-8 items-center"
+            >
+              {log}
+            </div>
+          ))}
+        </div>
       </div>
     </main>
-  )
+  );
 }
