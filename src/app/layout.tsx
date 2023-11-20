18a365e61965767d71fcb31161d49411ef4c1c4b
Vector <vector9527@proton.me>
2023-11-20 12:59:35 +0800

    feat: Add @vercel/analytics package

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -13,7 +14,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+
+        <Analytics />
+      </body>
     </html>
   );
 }
